827a137f43851234b38a9d3b47cfa779e0dd99ec
Sandanitin <sandanithin2@gmail.com>
2025-09-04 18:24:26 +0530

    j

--- ADMIN/src/App.jsx
+++ ADMIN/src/App.jsx
@@ -1,31 +1,18 @@
-import { useEffect } from 'react';
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import LoadingSpinner from "./components/common/LoadingSpinner";
-
-// Import components directly
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-
-// Loading component
-const PageLoader = () => (
-  <div className="flex items-center justify-center min-h-[60vh]">
-    <LoadingSpinner size="large" />
-  </div>
-);
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 const App = () => {
-  const location = useLocation();
-  
   return (
-    <div className="app-container">
+    <>
       <Toaster position="top-right" />
       <Navbar />
-      <main className="flex-grow">
+      <main className="min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -34,7 +21,7 @@ const App = () => {
         </Routes>
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
